90543b6ab707f2cd48dbcad66e106aceb20a33a7
naluboluraja-5637 <nalubolurajasekhar7386@gmail.com>
2025-03-26 11:13:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0ddc798b3f1a5117e"
+count = 4
+ami = "ami-08b5b3a93ed654d19"
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "virginiakeypair"
+vpc_security_group_ids = ["sg-0eceb708685fbc8d4"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
